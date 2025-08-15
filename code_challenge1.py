a8b937f1495d01b70516609497097fa6f08fecb7
trixelplata79-beep <trixelplata79@gmail.com>
2025-08-15 14:19:36 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,2 @@
+name = input("Enter your name: ")
+print("\t\t\t\t\t\t*\n\t\t\t\t\t*\t\t*\n\t\t\t\t*\t\t\t\t*\n\t\t\t*\t\t\t\t\t\t*\n\t\t*\t\t\tHi" ,name, "\t\t\t\t*\n\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t*")
