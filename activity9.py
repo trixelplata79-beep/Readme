6477d71b889223a613180fb606a1fbf7f12eaf7d
trixelplata79-beep <trixelplata79@gmail.com>
2025-08-22 14:09:33 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,12 @@
+a = 5
+b = 5
+
+c = 6
+d = 5
+username = 'trixel'
+password = 'plata'
+print((username == 'trixel') and
+(password == 'plata'))
+print(  (a > b) and (c < d)  )
+print ( (a > b) or  (c < d))
+print(not((a > b) or (c < d)))
