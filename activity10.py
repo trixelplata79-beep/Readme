0839b4857617dbdbea39a030dbbfa4d11239298d
trixelplata79-beep <trixelplata79@gmail.com>
2025-08-27 15:39:46 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,12 @@
+username = "Plata"
+password = "Trixel"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+ 
+if uname == username and pword == password:
+	print("ACCESS GRANTED")
+
+else:
+	print("ACCESS DENIED")
+
