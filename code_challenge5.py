4e11826fe36ca749648ca3299768aded1aedcb78
trixelplata79-beep <trixelplata79@gmail.com>
2025-09-16 11:23:47 +0800

    Create code_challenge5.py

---
+++ code_challenge5.py
@@ -0,0 +1,6 @@
+print("THE FACTORIAL PROGRAM")
+num = eval(input("Enter any number --> "))
+factorial = 1
+for plata in range(num, 0, -1):
+    factorial *= plata
+print("The factorial of", num," is ", factorial)
