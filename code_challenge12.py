c007d628c0b37490a2c2d6adc8de1ef98e77eb82
trixelplata79-beep <trixelplata79@gmail.com>
2025-10-23 14:58:51 +0800

    Create code_challenge12.py

---
+++ code_challenge12.py
@@ -0,0 +1,8 @@
+for plata in range(1, 10, 1):
+      for xyy in range(10, plata, -1):
+              print(" ", end=" ")
+      for trix in range(plata, 0, -1):
+              print(trix, end=" ")
+      for platatops in range(2, plata + 1, 1):
+              print(platatops, end=" ")
+      print( )
