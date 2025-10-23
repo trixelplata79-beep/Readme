dcfe8a594228d4cec81ac1d0b983c8f66a0b4e7e
trixelplata79-beep <trixelplata79@gmail.com>
2025-10-23 14:08:25 +0800

    Create code_challenge 11.py

---
+++ code_challenge 11.py
@@ -0,0 +1,11 @@
+print("\t\t *" ,end=" ")
+for x in range(1, 11, 1):
+    for p in range(10, x, -1):
+            print(" ", end =" ")
+    for t in range(1, x, 1):
+            print("*", end=" ")
+    for q in range(1, x, 1):
+             print("*", end=" ")
+    print()
+
+         
